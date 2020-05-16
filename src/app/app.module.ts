5ba8c96902b9b79d26894f1aa6f9ffa9f1b454d6
anandaknc <anandakumarnc@gmail.com>
2020-05-16 15:19:00 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { ImageCropperModule } from 'ngx-image-cropper';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MfsPhotoUploadComponent } from '../mfs-photo-upload/mfs-photo-upload.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,ImageCropperModule],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent ,MfsPhotoUploadComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
